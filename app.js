b30b44e121bab4dbc8c3c075a0a1814d1bbcd55b
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-12-07 02:27:29 -0300

    feat: camion

--- app.js
+++ app.js
@@ -1,20 +1,69 @@
 const express = require('express');
-const { cacheInit } = require('./middlewares/cacheExpeditious');
+const cors = require('cors');
+const fs = require('fs');
 const app = express();
+app.use(cors());
 
-app.get('/', cacheInit, (_req, res) => {
+const WriteJson = (data = {}, path = 'data.json') => {
+	data = JSON.stringify(data);
+	fs.writeFileSync(path, data);
+};
+const ReadJson = (path = 'data.json') => {
+	let rawdata = fs.readFileSync(path);
+	return JSON.parse(rawdata);
+};
+WriteJson({ play: 0 });
+WriteJson({ alert: 0 }, 'dataalert.json');
+console.log(ReadJson());
+
+app.use(express.json());
+app.get('/api', (_req, res) => {
 	try {
-		setTimeout(() => {
-			res.json({
-				msg: 'Hola mundo',
-			});
-		}, 4000);
+		const resp = ReadJson()?.play || 0;
+		console.log('get');
+		res.send(String(resp));
 	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: error.message });
+	}
+});
+app.get('/api/alert', (_req, res) => {
+	try {
+		const resp = ReadJson('dataalert.json')?.alert || 0;
+		console.log('get');
+		res.send(String(resp));
+	} catch (error) {
+		console.log(error);
 		res.status(500).json({ error: error.message });
 	}
 });
 
-const port = 3000;
+app.post('/api', (req, res) => {
+	try {
+		const { play } = req.body;
+		WriteJson({ play });
+		const resp = `Play now is ${play}`;
+		console.log(resp);
+		res.send(String(resp));
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: error.message });
+	}
+});
+app.post('/api/alert', (req, res) => {
+	try {
+		const { alert } = req.body;
+		WriteJson({ alert }, 'dataalert.json');
+		const resp = `Alert now is ${alert}`;
+		console.log(resp);
+		res.send(String(resp));
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: error.message });
+	}
+});
+
+const port = 8088;
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
